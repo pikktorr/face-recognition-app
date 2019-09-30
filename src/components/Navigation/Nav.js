1680b607fbc507ba001a16f74c55e4d465ce7e5c
pikktorr <50754130+pikktorr@users.noreply.github.com>
2019-09-30 21:57:07 +0200

    first commit

---
+++ src/components/Navigation/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return(
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className='f4 dim link pa2 ma3 ba b--light-blue bw1 br4 light-blue pointer'>Sign Out</p>
+        </nav>
+    );
+};
+
+export default Navigation;
