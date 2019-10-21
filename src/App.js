d1a51fa9ebba7b5ce15d5bdb2d86828461de08db
pikktorr <50754130+pikktorr@users.noreply.github.com>
2019-10-21 17:53:08 +0200

    code cleaning

--- src/App.js
+++ src/App.js
@@ -10,11 +10,6 @@ import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import "tachyons";
 import Particles from "react-particles-js";
 
-import Clarifai from "clarifai";
-const app = new Clarifai.App({
-  apiKey: "879bba8058df43be82c4a8e74ef27eaf"
-});
-
 const particlesOptions = {
   particles: {
     number: {
@@ -81,8 +76,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch("http://localhost:3000/image", {
