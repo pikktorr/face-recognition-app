456c307cf85d8e0d27833f81f87f34b11bafc253
pikktorr <50754130+pikktorr@users.noreply.github.com>
2019-10-21 18:49:03 +0200

    test

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
     return (
         <div className="center">
             <div className="absolute pb4">
@@ -13,7 +13,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
                 width="500px"
                 height="auto"
             />
-            <div
+            (<div
                 className="bounding-box"
                 style={{
                     top: box.topRow,
@@ -21,7 +21,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
                     bottom: box.bottomRow,
                     left: box.leftCol
                 }}
-            ></div>
+            ></div>)
             </div>
         </div>
     );
